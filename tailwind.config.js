4684261cba03c28c22fe2579d84457fd42edc909
Vidhi Jain <HARSH.TCA1909018+1@tmu.ac.in>
2024-07-29 18:48:07 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ export default {
         "Yeseva-One": ["Yeseva One", "sans-serif"],
         "Work-Sans": ["Work Sans", "sans-serif"],
       },
-    },
+      maxWidth: {
+        '7xl': '80rem', // default 1280px
+        '8xl': '90rem', // adding 1440px
+        '9xl': '100rem', // adding 1600px
+      }
+    }
   },
   plugins: [],
 };
