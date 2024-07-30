6147c54235129c7d4f4c0703ca212d3dc5ad3d50
Vidhi Jain <HARSH.TCA1909018+1@tmu.ac.in>
2024-07-31 02:13:14 +0530

    Add files via upload

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+
 import Aboutus from "../components/Aboutus";
 import Hero from "../components/Hero";
 import Legacy from "../components/Legacy";
 import OurDoctors from "../components/OurDoctors";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Responsive from '../components/Carousal2';
 export const LandingPage = () => {
   return (
     <>
